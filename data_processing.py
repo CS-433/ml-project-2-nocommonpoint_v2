21e12290686451a45dd0d6e382a90f28f27e90fe
julien <julien.mettler@epfl.ch>
2022-12-07 10:29:10 +0100

    Modify train

--- data_processing.py
+++ data_processing.py
@@ -5,69 +5,89 @@ import numpy as np
 
 
 # regression|classification & value|diff
-def load_phq9_targets(phq9_path, type='classification', target='value', derived_sum=True, 
-                    classes_dict=None):
+def load_phq9_targets(
+    phq9_path,
+    type="classification",
+    target="value",
+    derived_sum=True,
+    classes_dict=None,
+):
     csv = pd.read_csv(phq9_path)
 
     # TODO: derived sum is used in the csv right now
 
-#     if derived_sum:
-#         s = csv.loc[:, 'phq9_1':'phq9_9'].sum(axis=1)
-#     else:
-#         s = csv['phq9_sum']
+    #     if derived_sum:
+    #         s = csv.loc[:, 'phq9_1':'phq9_9'].sum(axis=1)
+    #     else:
+    #         s = csv['phq9_sum']
 
     target_diff = False
-    if target == 'diff':
+    if target == "diff":
         target_diff = True
 
-    if type == 'regression':
-        target_col = 'phq9_sum_diff' if target_diff else 'phq9_sum'
-    elif type == 'classification':
-        target_col = 'phq9_level_diff' if target_diff else 'phq9_level'
-        if not(target_diff) and classes_dict is not None:
-            target_col = 'phq9_sum'
+    if type == "regression":
+        target_col = "phq9_sum_diff" if target_diff else "phq9_sum"
+    elif type == "classification":
+        target_col = "phq9_level_diff" if target_diff else "phq9_level"
+        if not (target_diff) and classes_dict is not None:
+            target_col = "phq9_sum"
             csv[target_col] = csv[target_col].apply(lambda x: classes_dict[x])
         else:
             csv[target_col] -= 1
     else:
-        raise ValueError(f'Invalid PHQ9 target type {type}')
+        raise ValueError(f"Invalid PHQ9 target type {type}")
 
-    csv.rename(columns={'phq9Date': 'date', target_col: 'target'}, inplace=True)
+    csv.rename(columns={"phq9Date": "date", target_col: "target"}, inplace=True)
 
-    csv.drop(csv.columns.difference(['participant_id', 'date', 'target']), axis=1, inplace=True)
+    csv.drop(
+        csv.columns.difference(["participant_id", "date", "target"]),
+        axis=1,
+        inplace=True,
+    )
 
     if target_diff:
-        csv.dropna(axis=0, inplace=True) # remove NaN rows, useful for target_diff
+        csv.dropna(axis=0, inplace=True)  # remove NaN rows, useful for target_diff
 
     return csv
+
 
 def load_locations(loc_path):
     # Nothing superfluous. Nice!
     return pd.read_csv(loc_path)
 
+
 def load_demographics(dem_path):
     csv = pd.read_csv(dem_path)
 
     # Remove some extra columns
-    return csv.drop(['Unnamed: 0', 'startdate', 'study'], axis=1)
+    return csv.drop(["Unnamed: 0", "startdate", "study"], axis=1)
+
 
 def load_passive_mobility(pm_path):
     csv = pd.read_csv(pm_path)
 
-    csv.drop(['week'], axis=1, inplace=True)
+    csv.drop(["week"], axis=1, inplace=True)
 
-    csv.came_to_work = csv.came_to_work.astype('float')
-    
-    csv.rename(columns={'dt_passive': 'date'}, inplace=True)
+    csv.came_to_work = csv.came_to_work.astype("float")
+
+    csv.rename(columns={"dt_passive": "date"}, inplace=True)
 
     return csv
-    
+
+
 def load_passive_phone(pf_path):
     csv = pd.read_csv(pf_path)
 
-    return csv.drop(['week'], axis=1)
+    return csv.drop(["week"], axis=1)
 
-def combine(phq9: pd.DataFrame, dailies=None, constants=None, prev_phq9=False, daily_reduction=['mean', 'std']):
+
+def combine(
+    phq9: pd.DataFrame,
+    dailies=None,
+    constants=None,
+    prev_phq9=False,
+    daily_reduction=["mean", "std"],
+):
     """
     dailies: (name, csv) sequence, csv's are expected to have participant_id and date
     constants: (name, csv) sequence too, csv's are expected to have participant_id only
@@ -80,13 +100,13 @@ def combine(phq9: pd.DataFrame, dailies=None, constants=None, prev_phq9=False, d
     # 3.00  718     333
     # 5.00  NaN     NaN
     # NaN   333     444
-    # NaN   NaN     NaN 
+    # NaN   NaN     NaN
     # NaN   444     555
-    # 4.00  NaN     NaN 
+    # 4.00  NaN     NaN
     # becomes (with mean reduction):
     # phq9  daily0  daily1
     # 3.00  718     333
-    # 5.00  
+    # 5.00
     # 4.00  444     555
 
     if dailies is None:
@@ -99,11 +119,11 @@ def combine(phq9: pd.DataFrame, dailies=None, constants=None, prev_phq9=False, d
     # Dailies should be (name, csv) pairs.
 
     # Also, let's add an extra column has_* to check for daily attributes later
-    has_keys = ['has_phq9']
-    phq9['has_phq9'] = True
+    has_keys = ["has_phq9"]
+    phq9["has_phq9"] = True
 
     for name, csv in dailies:
-        k = f'has_{name}'
+        k = f"has_{name}"
         csv[k] = True
         has_keys.append(k)
 
@@ -111,54 +131,57 @@ def combine(phq9: pd.DataFrame, dailies=None, constants=None, prev_phq9=False, d
     # and those who don't exist for that date get NaNs
     daily_merged = phq9
     for name, csv in dailies:
-        daily_merged = daily_merged.merge(csv, on=['participant_id', 'date'], how='outer')
-    daily_merged.sort_values(by=['participant_id', 'date'], inplace=True)
+        daily_merged = daily_merged.merge(
+            csv, on=["participant_id", "date"], how="outer"
+        )
+    daily_merged.sort_values(by=["participant_id", "date"], inplace=True)
 
     # To make things look a bit cleaner, replace NaN with False for has_* columns
     for has_key in has_keys:
         daily_merged[has_key].fillna(False, inplace=True)
 
     if prev_phq9:
-        shifted_target = phq9.groupby('participant_id').apply(lambda x: x['target'].shift(1))
+        shifted_target = phq9.groupby("participant_id").apply(
+            lambda x: x["target"].shift(1)
+        )
         shifted_target.fillna(0, inplace=True)
-        daily_merged['prev_target'] = shifted_target.reset_index(drop=True)
+        daily_merged["prev_target"] = shifted_target.reset_index(drop=True)
 
     # Now we need to do the reduction. Now, I'm sure there is some sort of
 
-    # use a closure for grouping so we can cache some variables 
-    
+    # use a closure for grouping so we can cache some variables
+
     # note how to aggregate each column
     # we keep the last value for target and date (which is the phq9 date)
     # use the max for has_* keys so that it is 1 if at least one row is 1
     # the others are features, and we aggregate them with mean and std (daily_reduction)
 
     def has_filt(col):
-        return col.startswith('has_')
-    
+        return col.startswith("has_")
+
     def last_filt(col):
-        return col in ('target', 'date', 'prev_target')
+        return col in ("target", "date", "prev_target")
 
     def feature_filt(col):
-        return col != 'participant_id' and not (has_filt(col) or last_filt(col))
-   
+        return col != "participant_id" and not (has_filt(col) or last_filt(col))
 
     cols = daily_merged.columns
     aggdict = {
-        **{c: 'last' for c in cols if last_filt(c)},
-        **{c: 'max' for c in cols if has_filt(c)},
-        **{c: daily_reduction for c in cols if feature_filt(c)}
+        **{c: "last" for c in cols if last_filt(c)},
+        **{c: "max" for c in cols if has_filt(c)},
+        **{c: daily_reduction for c in cols if feature_filt(c)},
     }
 
     # now, build a rename dict to quickly rename columns
     rename_dict = {}
     for c in cols:
         if last_filt(c):
-            rename_dict[c, 'last'] = c
+            rename_dict[c, "last"] = c
         elif has_filt(c):
-            rename_dict[c, 'max'] = c
+            rename_dict[c, "max"] = c
         else:
             for reduct in daily_reduction:
-                rename_dict[c, reduct] = c + '_' + reduct
+                rename_dict[c, reduct] = c + "_" + reduct
 
     # finally, the grouping closure
     def _group_and_reduce_phq9(df):
@@ -167,7 +190,7 @@ def combine(phq9: pd.DataFrame, dailies=None, constants=None, prev_phq9=False, d
         aggframe.columns = [rename_dict[c] for c in aggframe.columns.to_flat_index()]
         return aggframe
 
-    pgrp = daily_merged.groupby('participant_id')
+    pgrp = daily_merged.groupby("participant_id")
     daily_rows = pgrp.apply(_group_and_reduce_phq9)
 
     # Need to post_process the daily_rows a bit
@@ -183,15 +206,16 @@ def combine(phq9: pd.DataFrame, dailies=None, constants=None, prev_phq9=False, d
 
     # Now, append the constants to each row
     for const in constants:
-        daily_rows = daily_rows.merge(const, on='participant_id')
+        daily_rows = daily_rows.merge(const, on="participant_id")
 
     # Add previous phq9 as context
-#     if prev_phq9:
-#         p = phq9.groupby('participant_id').apply(
-#             lambda x: pd.concat(
-#                 (x, x.rename({'target': 'prev_target'}, axis=1)['prev_target'].shift(1)), axis=1))
+    #     if prev_phq9:
+    #         p = phq9.groupby('participant_id').apply(
+    #             lambda x: pd.concat(
+    #                 (x, x.rename({'target': 'prev_target'}, axis=1)['prev_target'].shift(1)), axis=1))
 
     return daily_rows, daily_merged
+
 
 def _group_by_phq9(df):
     # Assign a different group number to each consecutive phq9 'block'
@@ -204,30 +228,37 @@ def _group_by_phq9(df):
             c += 1
     return groups
 
+
 def rf_preprocess(df):
-    target_columns = [c for c in df.columns if c.startswith('has_')]
+    target_columns = [c for c in df.columns if c.startswith("has_")]
     return df.drop(target_columns, axis=1)
 
+
 def xy_split(csv):
-    y = csv['target'].to_numpy()
-    x = csv.drop(csv.columns.intersection(['target', 'participant_id', 'date']), axis=1).to_numpy()
+    y = csv["target"].to_numpy()
+    x = csv.drop(
+        csv.columns.intersection(["target", "participant_id", "date"]), axis=1
+    ).to_numpy()
     return x, y
 
-def train_test_split_participant(csv, test_size=0.1, test_take_first=0, random_state=None):
+
+def train_test_split_participant(
+    csv, test_size=0.1, test_take_first=0, random_state=None
+):
     participants = csv.participant_id.unique()
     np.random.seed(random_state)
     np.random.shuffle(participants)
     cutoff = int(np.ceil(participants.size * test_size))
-    
+
     train_csv = csv[csv.participant_id.isin(participants[cutoff:])]
     test_csv = csv[csv.participant_id.isin(participants[:cutoff])]
 
     if test_take_first > 0:
-#         if 'date' in test_csv.columns:
-#             test_csv.sort_values(by=['participant_id', 'date'], inplace=True)
+        #         if 'date' in test_csv.columns:
+        #             test_csv.sort_values(by=['participant_id', 'date'], inplace=True)
 
-        train_part = test_csv.groupby('participant_id').head(test_take_first)
-        test_part = test_csv.groupby('participant_id').tail(-test_take_first)
+        train_part = test_csv.groupby("participant_id").head(test_take_first)
+        test_part = test_csv.groupby("participant_id").tail(-test_take_first)
 
         final_test_csv = test_part
         final_train_csv = pd.concat((train_csv, train_part))
@@ -237,8 +268,10 @@ def train_test_split_participant(csv, test_size=0.1, test_take_first=0, random_s
         train_pct = 100 * train_count / (train_count + test_count)
         test_pct = 100 * test_count / (train_count + test_count)
 
-        print(f'After test_take_first={test_take_first}, have {train_pct:.2f}% ({train_count}) '
-              f'vs. {test_pct:.2f} ({test_count}) participants in train/test set')
+        print(
+            f"After test_take_first={test_take_first}, have {train_pct:.2f}% ({train_count}) "
+            f"vs. {test_pct:.2f} ({test_count}) participants in train/test set"
+        )
     else:
         final_test_csv, final_train_csv = (test_csv, train_csv)
 
@@ -247,9 +280,9 @@ def train_test_split_participant(csv, test_size=0.1, test_take_first=0, random_s
 
     return x_train, x_test, y_train, y_test
 
+
 # def _reduce_rows(rows, method='mean'):
 #     # rows are sorted by date (and do not contain participant ids)
 #     # now, we just combine everything between has_phq9 rows, somehow
 #     reduced = []
 #     for row in rows:
-        
--- train.py
+++ train.py
@@ -6,146 +6,147 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
-
-
 from data_processing import *
 import data_processing as dp
 
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.feature_selection import SelectFromModel
 
-DATADIR = Path('data')
+DATADIR = Path("data")
+
+
+def rmse(x, y):
+    return np.sqrt(((x - y) ** 2).mean())
+
 
 def train(
-    TYPE='classification', TARGET='value', SPLIT_BY_PARTICIPANT=False, 
-    TEST_TAKE_FIRST=0, SEED=550, return_csv=False, 
-    feature_selection=False, classes_dict=None):
-    
-    phq9 = dp.load_phq9_targets(DATADIR / 'df_phq9.csv', type=TYPE, target=TARGET, classes_dict=classes_dict)
-    locations = load_locations(DATADIR / 'df_location_ratio.csv')
-    demographics = load_demographics(DATADIR / 'df_demographics.csv')
-    #mobility = load_passive_mobility(DATADIR / 'df_passive_mobility_features.csv')
-    #phone = load_passive_phone(DATADIR / 'df_passive_phone_communication_features_brighten_v2.csv')
+    TYPE="classification",
+    TARGET="value",
+    SPLIT_BY_PARTICIPANT=False,
+    TEST_TAKE_FIRST=0,
+    SEED=550,
+    return_csv=False,
+    feature_selection=False,
+    classes_dict=None,
+    verbose=True,
+):
+
+    phq9 = dp.load_phq9_targets(
+        DATADIR / "df_phq9.csv", type=TYPE, target=TARGET, classes_dict=classes_dict
+    )
+    locations = load_locations(DATADIR / "df_location_ratio.csv")
+    demographics = load_demographics(DATADIR / "df_demographics.csv")
+    # mobility = load_passive_mobility(DATADIR / 'df_passive_mobility_features.csv')
+    # phone = load_passive_phone(DATADIR / 'df_passive_phone_communication_features_brighten_v2.csv')
 
     combined, merge_result = dp.combine(
-        phq9, 
+        phq9,
         dailies=[
-            ('locations', locations), 
+            ("locations", locations),
             # ('mobility', mobility),
             # ('phone', phone)
-        ], 
-        constants=[
-            demographics
         ],
-        prev_phq9=False
+        constants=[demographics],
+        prev_phq9=False,
     )
     combined = dp.rf_preprocess(combined)
 
     if return_csv:
         return combined
-    
+
     if SPLIT_BY_PARTICIPANT:
-        x_train, x_test, y_train, y_test = dp.train_test_split_participant(combined, 0.15, random_state=SEED,
-                                                                           test_take_first=TEST_TAKE_FIRST)
+        x_train, x_test, y_train, y_test = dp.train_test_split_participant(
+            combined, 0.15, random_state=SEED, test_take_first=TEST_TAKE_FIRST
+        )
     else:
         x, y = dp.xy_split(combined)
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.15, random_state=SEED, stratify=y)
+        x_train, x_test, y_train, y_test = train_test_split(
+            x, y, test_size=0.15, random_state=SEED, stratify=y
+        )
 
-    print('Train set shape:', x_train.shape)
-    print('Test set shape:', x_test.shape)
     n = len(y_train) + len(y_test)
     train_pct = len(y_train) / n * 100
     test_pct = len(y_test) / n * 100
-    print(f'Ratio: {train_pct:.2f}%/{test_pct:.2f}%')
-    print()
+    if verbose:
+        print("Train set shape:", x_train.shape)
+        print("Test set shape:", x_test.shape)
+        print(f"Ratio: {train_pct:.2f}%/{test_pct:.2f}%")
+        print()
 
-
-    if TYPE == 'regression':
-        def rmse(x, y):
-            return np.sqrt(((x - y)**2).mean())
-
+    if TYPE == "regression":
         model = RandomForestRegressor(n_estimators=300, n_jobs=-1, random_state=SEED)
-        model.fit(x_train, y_train)
-
-        train_rmse = rmse(y_train, model.predict(x_train))
-        test_rmse = rmse(y_test, model.predict(x_test))
-        print(f'Train set RMSE: {train_rmse:.4f}')
-        print(f'Test set RMSE:  {test_rmse:.4f}')
-
-        train_score = model.score(x_train, y_train)
-        test_score = model.score(x_test, y_test)
-        print(f'Train score:', train_score)
-        print(f'Test score:', test_score)
-        #return train_score, test_score
-        
-        if feature_selection == True:
-            combined = combined.drop(columns=['participant_id', 'date', 'target'])
-            feature_importances = pd.DataFrame({'feature': combined.columns, 'importance': model.feature_importances_})
-            feature_importances = feature_importances.sort_values('importance', ascending=False).reset_index(drop=True)
-            #print(feature_importances)
-            plt.figure(figsize=(10, 10))
-            sns.barplot(x='importance', y='feature', data=feature_importances)
-            plt.show()
-
-            print('-'*25)
-            print("After feature selection:")
-            sfm = SelectFromModel(model, threshold=0.01, prefit=True)
-            x_train_new = sfm.transform(x_train)
-            x_test_new = sfm.transform(x_test)
-            print('Train set shape:', x_train_new.shape)
-            print('Test set shape:', x_test_new.shape)
-            print()
-            sfm.estimator.fit(x_train_new, y_train)
-            train_score = sfm.estimator.score(x_train_new, y_train)
-            test_score = sfm.estimator.score(x_test_new, y_test)
-            print(f'Train score:', train_score)
-            print(f'Test score:', test_score)
-
-
-    elif TYPE == 'classification':
+    elif TYPE == "classification":
         model = RandomForestClassifier(n_estimators=300, n_jobs=-1, random_state=SEED)
-        model.fit(x_train, y_train)
+    model.fit(x_train, y_train)
 
-        train_acc = 100 * model.score(x_train, y_train)
-        test_acc = 100 * model.score(x_test, y_test)
-        print(f'Train set accuracy: {train_acc:.2f}%') 
-        print(f'Test set accuracy:  {test_acc:.2f}%')
+    train_rmse = rmse(y_train, model.predict(x_train))
+    test_rmse = rmse(y_test, model.predict(x_test))
+    train_score = 100 * model.score(x_train, y_train)
+    test_score = 100 * model.score(x_test, y_test)
+    if verbose:
+        print(f"Train RMSE: {train_rmse:.4f}")
+        print(f"Test RMSE:  {test_rmse:.4f}")
+        print(f"Train score: {train_score:.2f}%")
+        print(f"Test score:  {test_score:.2f}%")
 
-        if feature_selection == True:
-            combined = combined.drop(columns=['participant_id', 'date', 'target'])
-            feature_importances = pd.DataFrame({'feature': combined.columns, 'importance': model.feature_importances_})
-            feature_importances = feature_importances.sort_values('importance', ascending=False).reset_index(drop=True)
-            #print(feature_importances)
-            plt.figure(figsize=(10, 10))
-            sns.barplot(x='importance', y='feature', data=feature_importances)
-            plt.show()
+    train_score_sel, test_score_sel = (
+        feature_selection_results(
+            model, combined, x_train, y_train, x_test, y_test, verbose=verbose
+        )
+        if feature_selection == True
+        else (None, None)
+    )
 
-            print('-'*25)
-            print("After feature selection:")
-            sfm = SelectFromModel(model, threshold=0.01, prefit=True)
-            x_train_new = sfm.transform(x_train)
-            x_test_new = sfm.transform(x_test)
-            print('Train set shape:', x_train_new.shape)
-            print('Test set shape:', x_test_new.shape)
-            print()
-            sfm.estimator.fit(x_train_new, y_train)
-            train_acc = 100 * sfm.estimator.score(x_train_new, y_train)
-            test_acc = 100 * sfm.estimator.score(x_test_new, y_test)
-            print(f'Train set accuracy: {train_acc:.2f}%') 
-            print(f'Test set accuracy:  {test_acc:.2f}%')
-                
-        # y_full = np.concatenate((y_train, y_test))
-        # random_acc = 100 * (y_full == np.random.randint(0, 5, size=y_full.shape)).mean()
-        # print(f'Random full accuracy: {random_acc:.2f}%')
+    return train_score, test_score, train_score_sel, test_score_sel
 
-        # figs, axes = plt.subplots(1, 2, figsize=(16, 6))
-        
-        # print('Train set:')
-        # train_bal, train_mean = metrics.accuracy_info(y_train, model.predict(x_train), prefix='Training', ax=axes[0])
+    # y_full = np.concatenate((y_train, y_test))
+    # random_acc = 100 * (y_full == np.random.randint(0, 5, size=y_full.shape)).mean()
+    # print(f'Random full accuracy: {random_acc:.2f}%')
 
-        # print('Test set:')
-        # test_bal, test_mean = metrics.accuracy_info(y_test, model.predict(x_test), prefix='Test', ax=axes[1])
-        
-        #return train_bal, test_bal
+    # figs, axes = plt.subplots(1, 2, figsize=(16, 6))
+
+    # print('Train set:')
+    # train_bal, train_mean = metrics.accuracy_info(y_train, model.predict(x_train), prefix='Training', ax=axes[0])
+
+    # print('Test set:')
+    # test_bal, test_mean = metrics.accuracy_info(y_test, model.predict(x_test), prefix='Test', ax=axes[1])
+
+    # return train_bal, test_bal
+
+
+def feature_selection_results(
+    model, combined, x_train, y_train, x_test, y_test, verbose=True
+):
+    feature_importances = model.feature_importances_
+
+    sfm = SelectFromModel(model, threshold=0.01, prefit=True)
+    x_train_new = sfm.transform(x_train)
+    x_test_new = sfm.transform(x_test)
+    sfm.estimator.fit(x_train_new, y_train)
+    train_score_sel = 100 * sfm.estimator.score(x_train_new, y_train)
+    test_score_sel = 100 * sfm.estimator.score(x_test_new, y_test)
+
+    if verbose:
+        print()
+        print("After feature selection:")
+        print("Train set shape:", x_train_new.shape)
+        print("Test set shape:", x_test_new.shape)
+        print()
+        print(f"Train score: {train_score_sel:.2f}%")
+        print(f"Test score:  {test_score_sel:.2f}%")
+
+        combined = combined.drop(columns=["participant_id", "date", "target"])
+        feature_importances = (
+            pd.DataFrame(
+                {"feature": combined.columns, "importance": feature_importances}
+            )
+            .sort_values("importance", ascending=False)
+            .reset_index(drop=True)
+        )
+
+        plt.figure(figsize=(10, 10))
+        sns.barplot(x="importance", y="feature", data=feature_importances)
+        plt.show()
+
+    return train_score_sel, test_score_sel
