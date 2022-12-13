2bf7c850f46dc6a1eefe95042e903a47a6219a84
Deniz Mercadier Sayin <denizmsayin@gmail.com>
2022-12-13 15:45:42 +0100

    added training, cross validation and whatnot... sorry for doing all at once!

--- metrics.py
+++ metrics.py
@@ -14,12 +14,24 @@ def per_class_accuracy(y, y_pred):
         recalls.append(rec)
     return np.array(recalls)
 
+def per_class_corrects_totals(y, y_pred):
+    min_label = y.min()
+    max_label = y.max()
+    corrects, totals = [], []
+    for l in range(min_label, max_label + 1):
+        in_class = y == l
+        tp = (in_class & (y_pred == l)).sum()
+        total = in_class.sum()
+        corrects.append(tp)
+        totals.append(total)
+    return np.array(corrects), np.array(totals)
+
 def accuracy_info(y, y_pred, plot=True, prefix='', ax=None):
     y = y.astype(np.int64)
     y_pred = y_pred.astype(np.int64)
     mean_acc = (y == y_pred).mean()
     per_class_acc = per_class_accuracy(y, y_pred)
-    balanced_acc = per_class_acc.mean()
+    balanced_acc = per_class_acc[np.isfinite(per_class_acc)].mean()
     if plot:
         if ax is None:
             fig, ax = plt.subplots()
@@ -30,5 +42,10 @@ def accuracy_info(y, y_pred, plot=True, prefix='', ax=None):
         ax.grid()
     print(f'Mean accuracy: {100 * mean_acc:.3f}%')
     print(f'Balanced accuracy: {100 * balanced_acc:.3f}%')
-    return balanced_acc, per_class_acc
+    return balanced_acc, mean_acc
+
+def correct_count_info(y, y_pred):
+    y = y.astype(np.int64)
+    y_pred = y_pred.astype(np.int64)
+    return per_class_corrects_totals(y, y_pred)
 
